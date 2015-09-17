28c44d55ad9666321417a18aa42674bcc72398af
marilia <maricardosop@yahoo.com.br>
2015-09-17 14:03:52 -0300

    foram add listas com os 5 maiores e 5 menores valores

--- bubble-sort.py
+++ bubble-sort.py
@@ -3,19 +3,26 @@
 N = 20
 # Indica que a lista possui todos esses elementos.
 lista = [11,18,3,1,16,12,6,19,5,0,14,4,17,9,13,7,10,15,2,8] 
-print(lista)
+print("Lista original:", lista)
 # Indica que o valor de i começa do 0 e vai até 18, de um em um.
 for i in range(0, N-1, 1):
 # Indica que o valor de j começa em 1, variando com os valores de i, até 19, de um em um.
     for j in range(i+1, N, 1):
 # Condiciona que: se a lista i for maior que a lista j, ele deve executar a mudança abaixo.
-	    if lista [i] > lista [j]: 
+        if lista[i] > lista[j]: 
 # Coloca a lista i num aquivo temporario, chamado temp.
-		    temp = lista [i] 
+            temp = lista[i] 
 # Coloca a lista j no lugar da lista i.
-			lista [i] = lista [j] 
+            lista[i] = lista[j] 
 # Coloca o arquivo temporario na lista j.
-			lista [j] = temp
+            lista[j] = temp
 # Escreve no terminal a lista ordenada.			
-			    print(lista) 
-					
+print("Lista em ordem crescente:", lista)
+# Indica a lista no intervalo de 14 a 19.
+lista[N-5:N]
+# Imprime a lista no intervalo de 14 a 19.
+print("Cinco maiores valores:", lista[N-5:N])
+# Indica a lista no intervalo de 0 a 4.
+lista[0:5]
+# Imprime a lista no intervalo de 0 a 4.
+print("Cinco menores valores:", lista[0:5])					
