96468073f59e8d926799cf22cb62fb5f059ed585
marilia cardoso <maricardosop@yahoo.com.br>
2015-09-23 16:14:52 -0300

    Foram gerados os graficos da lista original como bubble-inicio e o da lista ordenada como bubble-final, com os devidos titulos e nomes dos eixos.

--- bubble-sort.py
+++ bubble-sort.py
@@ -4,6 +4,7 @@ N = 20
 # Indica que a lista possui todos esses elementos.
 lista = [11,18,3,1,16,12,6,19,5,0,14,4,17,9,13,7,10,15,2,8] 
 print("Lista original:", lista)
+
 import matplotlib.pyplot as plt
 plt.figure()
 plt.plot(range(0, N, 1), lista, "ok")
@@ -12,6 +13,7 @@ plt.xlabel("indices")
 plt.ylabel("valores")
 plt.savefig("fig/bubble-inicio.png")
 plt.close()
+
 # Indica que o valor de i começa do 0 e vai até 18, de um em um.
 for i in range(0, N-1, 1):
 # Indica que o valor de j começa em 1, variando com os valores de i, até 19, de um em um.
@@ -26,6 +28,7 @@ for i in range(0, N-1, 1):
             lista[j] = temp
 # Escreve no terminal a lista ordenada.			
 print("Lista em ordem crescente:", lista)
+
 plt.figure()
 plt.plot(range(0, N, 1), lista, "ok")
 plt.title ("estado final")
@@ -33,6 +36,7 @@ plt.xlabel("indices")
 plt.ylabel("valores")
 plt.savefig("fig/bubble-final.png")
 plt.close()
+
 # Indica a lista no intervalo de 14 a 19.
 lista[N-5:N]
 # Imprime a lista no intervalo de 14 a 19.
